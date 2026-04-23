3e4cfc89fd0aa943d8677366f5d305acba3ed3bf
Hanyu Xu <hanyu.xu@aol.com>
2026-04-23 08:27:04 +0200

    Add camera calibration parameters to settings configuration

--- settings.py
+++ settings.py
@@ -4,3 +4,10 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 
 CAMERA_POSITION = DATA_DIR / "camera_position.csv"
+
+# camera calibration parameters
+
+# number of spatial pixels of the line scanner
+SPATIAL_PIXEL = 1312
+# time required [ms] for the scanner to transfer the captured frame to the PC
+FTT = 3.4
